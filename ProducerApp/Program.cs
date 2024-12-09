1738c3c45140f3613ed6a7a12dad74c2b07ab9a9
mostafa rezaie <mostafa.rezaie@gmail.com>
2024-12-09 23:17:37 +0330

    Add project files.

---
+++ ProducerApp/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using Helper;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("Hello, Please enter user phone number:");
+var phoneNumber = Console.ReadLine();
+Console.WriteLine("Please enter user email:");
+var email = Console.ReadLine();
+string exchangeName = "userAlertExchange";
+
+var connectionFactory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    UserName = "guest",
+    Password = "guest"
+};
+IConnection connection = await connectionFactory.CreateConnectionAsync();
+IChannel channel = await connection.CreateChannelAsync();
+await channel.ExchangeDeclareAsync(exchangeName, type: ExchangeType.Fanout, durable: true, autoDelete: false, arguments: null);
+
+var user = new User
+{
+    PhoneNumber = phoneNumber,
+    Email = email
+};
+var userJSON = JsonConvert.SerializeObject(user);
+
+await channel.BasicPublishAsync(exchange: exchangeName, routingKey: "", body: Encoding.UTF8.GetBytes(userJSON));
+
+Console.WriteLine("User informaions sent to queue.");
+Console.ReadKey();
